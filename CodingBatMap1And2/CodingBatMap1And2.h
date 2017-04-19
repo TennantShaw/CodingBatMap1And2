14dbc440f1f796b00aba6364390a508869ede57f
Tennant Shaw <tennantshaw@macs-mbp.lan>
2017-04-19 14:01:42 -0400

    Initial Commit

---
+++ CodingBatMap1And2/CodingBatMap1And2.h
@@ -0,0 +1,19 @@
+//
+//  CodingBatMap1And2.h
+//  CodingBatMap1And2
+//
+//  Created by Tennant Shaw on 4/19/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodingBatMap1And2.
+FOUNDATION_EXPORT double CodingBatMap1And2VersionNumber;
+
+//! Project version string for CodingBatMap1And2.
+FOUNDATION_EXPORT const unsigned char CodingBatMap1And2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodingBatMap1And2/PublicHeader.h>
+
+
